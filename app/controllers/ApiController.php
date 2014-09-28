18b200f211815070b428954049257c2cf0377c1a
Eduardo Reveles <me@osiux.ws>
2014-09-28 00:51:51 -0500

    Obtener info de sensor en API

--- app/controllers/ApiController.php
+++ app/controllers/ApiController.php
@@ -1,7 +1,100 @@
 <?php
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+
 class ApiController extends BaseController
 {
-    public function index()
+    protected $user;
+    protected $sensor;
+    protected $apikey;
+
+    public function __construct(User $user, Sensor $sensor, ApiKey $apikey)
+    {
+        parent::__construct();
+
+        $this->sensor   = $sensor;
+        $this->user     = $user;
+        $this->apikey   = $apikey;
+
+        $this->beforeFilter('@checkValidSensor', ['only' => ['sensorUpload']]);
+        $this->beforeFilter('@checkApiKey', ['only' => ['sensorInfo', 'locateSensors']]);
+    }
+
+    public function checkValidSensor($route, $request)
+    {
+        $apiKey     = Input::get('api_key');
+        $sensorId   = Input::get('sensor_id');
+
+        try {
+            $sensor = $this->sensor->where('uuid', '=', $sensorId)->firstOrFail();
+        }catch(ModelNotFoundException $e) {
+            return Response::json(array(
+                'success'   =>  false,
+                'error'     =>  array(
+                    'message'   =>  'Sensor not found.'
+                ),
+            ))->setCallback(Input::get('callback'));
+        }
+
+        if ( ! $sensor->user->apikey || $sensor->user->apikey->key != $apiKey ) {
+            return Response::json(array(
+                'success'   =>  false,
+                'error'     =>  array(
+                    'message'   =>  'Wrong API key.'
+                ),
+            ))->setCallback(Input::get('callback'));
+        }
+    }
+
+    public function checkApiKey($route, $request)
+    {
+        $apiKey     = Input::get('api_key');
+
+        try {
+            $key = $this->apikey->where('key', '=', $apiKey)->firstOrFail();
+        }catch(ModelNotFoundException $e) {
+            return Response::json(array(
+                'success'   =>  false,
+                'error'     =>  array(
+                    'message'   =>  'API Key not found.'
+                ),
+            ))->setCallback(Input::get('callback'));
+        }
+
+        if ( $key->user->apicalls <= 0 ) {
+            // TODO:
+        }
+    }
+
+    public function sensorUpload()
+    {
+        $temperature    =   Input::get('temperature');
+        $moisture       =   Input::get('moisture');
+        $altitude       =   Input::get('altitude');
+        $pressure       =   Input::get('pressure');
+        $sound          =   Input::get('noise');
+        $light          =   Input::get('light');
+
+        $created_at     =   Input::get('created_at');
+    }
+
+    public function sensorInfo($uuid = null)
+    {
+        try {
+            $sensor = $this->sensor->where('uuid', '=', $uuid)->firstOrFail();
+        }catch(ModelNotFoundException $e) {
+            return Response::json(array(
+                'success'   =>  false,
+                'error'     =>  array(
+                    'message'   =>  'Sensor not found.'
+                ),
+            ))->setCallback(Input::get('callback'));
+        }
+
+        return Response::json($sensor->toArray())
+                ->setCallback(Input::get('callback'));
+    }
+
+    public function locateSensors($latitude = null, $longitude = null, $areasize = '2')
     {
         //
     }
--- app/models/Sensor.php
+++ app/models/Sensor.php
@@ -9,6 +9,8 @@ class Sensor extends Eloquent
 
     protected $fillable = ['label', 'latitude', 'longitude'];
 
+    protected $hidden = ['user_id', 'id'];
+
     protected $rules = [
         'latitude'  => 'required',
         'longitude' => 'required',
@@ -22,5 +24,10 @@ class Sensor extends Eloquent
     public function getNameAttribute()
     {
         return empty($this->label) ? $this->uuid : sprintf('%s <span class="opaque">( %s )</span>', e($this->label), $this->uuid);
+    }
+
+    public function scopeActive($query)
+    {
+        return $query->where('status', '=', true);
     }
 }
