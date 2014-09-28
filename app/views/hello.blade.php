d51d0a240c9f8a535a9b5709199555d320f03b69
Luis Felipe Guzman <lfguzman@digitalartsnetwork.com.mx>
2014-09-28 09:33:58 -0500

    home con copies

--- app/views/hello.blade.php
+++ app/views/hello.blade.php
@@ -8,7 +8,7 @@
 	<div id="intro">
 		<div class="interior">
 			<div class="welcomeBox">
-				Open data sensor grid network feeded by sensors scatered around the city.
+				<span>Open data</span> sensor grid network feeded by sensors scatered around the city.
 			</div>
 			<img src="{{ asset('assets/img/misc/sensoraSimbolo.png') }}" />
 		</div>
@@ -20,14 +20,12 @@
 			<div class="large-12 columns">
 
 				<h2>What's sensora?</h2>
-				<p class="large-6 small-12 columns">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 				<p class="large-6 small-12 columns">
-					<img src="{{ asset('assets/img/animaciones/step01.gif') }}" />
+					A complete sensor grid network that provides precise open real time data.<br />
+					This data can be used for improving your locality
+				</p>
+				<p class="large-6 small-12 columns">
+					<img src="{{ asset('assets/img/animaciones/step02.gif') }}" />
 				</p>
 			</div>
 		</div>
@@ -38,14 +36,30 @@
 
 				<h2>How it works?</h2>
 				<p class="large-6 small-12 columns">
-					<img src="{{ asset('assets/img/animaciones/step02.gif') }}" />
+					<img src="{{ asset('assets/img/animaciones/step01.gif') }}" />
 				</p>
-				<p class="large-6 small-12 columns">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				<p class="large-6 small-12 columns">
+					A sensor collects enviromental data, such as temperature, atmospheric pressure, 
+					humity, light and noise intensity, altitude, plus others.<br />
+					These sensors send the data to the Open API so other users be able to consult it.
+				</p>
+			</div>
+		</div>
+	</div>
+
+	<div class="explicacion github">
+		<div class="row">
+			<div class="large-12 columns">
+
+				<h2>Open Source Data</h2>
+				<p class="large-6 small-12 columns">
+					<img src="{{ asset('assets/img/misc/github.png') }}" />
+				</p>
+				<p class="large-6 small-12 columns">
+					Data, platform and all the code is open source.<br />
+					Feel free to collaborate and improve it. We can do better things together.<br />
+					<a href="https://github.com/sensora" target="_blank">Collaborate on Github</a>
+				</p>
 			</div>
 		</div>
 	</div>
@@ -59,9 +73,10 @@
 					<div class="large-6 small-12 columns">
 						<h3>Improving the movility</h3>
 						<p>
-							How about an app which not only gives you a route to your destiny but also dodge the rainy clouds when possible. 
-							When geographical apps just gives you straight routes and wheater apps only say your city is under rain, Sensora based apps 
-							are a lot more specific because they could be able to tell you the exact location of the storm and its direction.
+							How about an app which not only gives you a route for your destination but also dodge the rainy clouds when possible. <br />
+							Where geographical apps just give you straight routes and the wheater apps only say your city is under rain, Sensora based apps 
+							are a lot more specific because they could be able to tell you the exact location of the storm and its direction.<br />
+							Enjoy a sunny bicycle ride.
 						</p>
 					</div>
 					<p class="large-6 small-12 columns">
@@ -88,17 +103,20 @@
 						<h3>Multiple and all kind of apps in every level</h3>
 						<p>
 							Everybody is able to use Sensora's API, this is an oportunity for all the people interested 
-							in create new apps.<br />Goverment, Bussinesses, Professional and Amateurs Developers. Everybody is invited 
-							just use your imagination and start enjoying the benefits of all the data available by Sensora
+							in create new apps.<br />
+							Goverment, Bussinesses, Professional and Amateurs Developers. Everybody is invited 
+							just use your imagination and start enjoying the benefits of all the available data by Sensora.
 						</p>
 						<p>
-							The sky is the limit (we'are not planning to install Sensora above the sky, yet).
+							
 						</p>
 					</div>
 					<p class="large-6 small-12 columns">
 						<img src="{{ asset('assets/img/animaciones/apps.gif') }}" />
 					</p>
 				</div>
+
+				<h2 class="lastQuote">The sky is the limit <span>(we have no plans to install Sensora above the sky... yet).</span></h2>
 
 			</div>
 		</div>
